b0875902b29bb0641f402006a6a8652db298c549
gladysuchola <ucholagladys@gmail.com>
2025-01-24 14:10:14 +0400

    add commit list

--- script.js
+++ script.js
@@ -15,27 +15,29 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     
-    function addTask(taskText, saveToStorage = true) {
+    function addTask() {
         
-        taskText = taskText.trim();
+        const taskText = taskInput.value.trim();
+    
+        
         if (taskText === "") {
             alert("Please enter a task.");
             return;
         }
-
+    
         
         const li = document.createElement('li');
         li.textContent = taskText;
-
     
+        
         const removeBtn = document.createElement('button');
         removeBtn.textContent = "Remove";
         removeBtn.className = 'remove-btn';
         removeBtn.onclick = function() {
             taskList.removeChild(li);
-            saveTasks();
+        
         };
-
+    
         li.appendChild(removeBtn);
         taskList.appendChild(li);
 
